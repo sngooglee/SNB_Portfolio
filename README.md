8a975e5bf96f036df2463101537c881f350f73e6
sngooglee <72295362+sngooglee@users.noreply.github.com>
2021-09-10 21:45:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# SNB_Portfolio
+# SNB-Web-Codes
+This is my pesonal portfolio website
+
+It stands on the three texture HTML, CSS and JavaScript.
+
+Home Screen
+
+![](images/Screenshot%20(62).png)
+![](images/Screenshot%20(63).png)
+![](images/Screenshot%20(64).png)
---
+++ app.js
@@ -0,0 +1,83 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundcolor = '#29323c';
+    }else{
+        header.style.backgroundcolor = 'transparent';
+    }
+});
+
+menu_item.forEach(item=>{
+    item.addEventListener('click',() => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+/* Cursor Effect  */
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Intresting", "fun", "a journey", "Life"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
